4bd3089b852c22a219f0797ea03fa648986e04e8
romantrickey-ops <roman.trickey@students.msswarriors.org>
2026-04-27 15:22:25 -0500

    Update index.js

--- index.js
+++ index.js
@@ -2,4 +2,5 @@ var counter = 100;
 
  var counter = 0;
 postfixIncrement = 
-document.getElementById("prefixIncrement").innerHTML = counter;
+document.getElementById("prefix-Increment").innerHTML = counter;
+document.getElementById("prefixIncrement").innerHTML = conuter;
